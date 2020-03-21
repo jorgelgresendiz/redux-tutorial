dbd84c2b094117aaa26761d34ad0746f7ffff665
Jorge Galvan <jorgegalvan@Jorges-Air.attlocal.net>
2020-03-21 15:15:45 -0500

    increased counter state by using a button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment } from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello dev!</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button>-</button>
+      {isLogged ? <h2>Valuable information i should not see</h2> : ''}
     </div>
   );
 }
