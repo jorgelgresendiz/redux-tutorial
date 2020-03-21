7373f699bbab711f5ae5bf2ae7bc30d31409808c
Jorge Galvan <jorgegalvan@Jorges-Air.attlocal.net>
2020-03-21 14:52:34 -0500

    add redux devtools extension to chrome to visualize state management

--- src/index.js
+++ src/index.js
@@ -4,42 +4,49 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
+import allReducer from './reducers';
 
-// STORE -> a globalized state used to store the data for access to all app components
+const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// import { createStore } from 'redux';
+
+// // STORE -> a globalized state used to store the data for access to all app components
 
 
-//ACTION -> describes what action is performed -> function that returns an object
-const increment = () => {
-    return {
-        type: 'INCREMENT'
-    }
-}
+// //ACTION -> describes what action is performed -> function that returns an object
+// const increment = () => {
+//     return {
+//         type: 'INCREMENT'
+//     }
+// }
 
-const decrement = () => {
-    return {
-        type: 'DECREMENT'
-    }
-}
+// const decrement = () => {
+//     return { 
+//         type: 'DECREMENT'
+//     }
+// }
 
-//REDUCER 
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-    }
-};
+// //REDUCER 
+// const counter = (state = 0, action) => {
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return state + 1;
+//         case 'DECREMENT':
+//             return state - 1;
+//     }
+// };
 
-let store = createStore(counter);
+// let store = createStore(counter);
 
-//Display on the console
-store.subscribe(() => console.log(store.getState()));
+// //Display on the console
+// store.subscribe(() => console.log(store.getState()));
 
-//DISPATCH
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(decrement());
+// //DISPATCH
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
+
+
 
 
 
