9be699c4cecbc0c1e93166f6322d588dccd31e80
Jorge Galvan <jorgegalvan@Jorges-Air.attlocal.net>
2020-03-21 15:22:47 -0500

    decreased counter state by using a button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { increment } from './actions';
+import { decrement } from './actions';
 
 function App() {
   const counter = useSelector(state => state.counter)
@@ -11,7 +12,7 @@ function App() {
     <div className="App">
       <h1>Counter {counter}</h1>
       <button onClick={() => dispatch(increment())}>+</button>
-      <button>-</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
       {isLogged ? <h2>Valuable information i should not see</h2> : ''}
     </div>
   );
