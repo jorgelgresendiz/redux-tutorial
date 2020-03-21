2d0726084139fe30202c2eaca8bcef8522f0d04e
Jorge Galvan <jorgegalvan@Jorges-Air.attlocal.net>
2020-03-21 14:19:56 -0500

    created counter that increases and decreases numbers by updating state via redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
 // STORE -> a globalized state used to store the data for access to all app components
 
-//ACTION -> checks what action is performed and alters the state (STORE)
 
-//REDUCER
+//ACTION -> describes what action is performed -> function that returns an object
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+//REDUCER 
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+    }
+};
+
+let store = createStore(counter);
+
+//Display on the console
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
